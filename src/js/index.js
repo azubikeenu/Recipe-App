8f20bfaeb0c2eb2467472549d4798346a2aefc97
Enu Azubike <enuazubike88@gmail.com>
2021-05-01 01:33:50 +0100

    set up babel config

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,5 @@
 import x from './test'
+const findMe = () => {
+    return "Hello";
+}
 console.log( x + " test the imported module" )
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HTMLWebPackPlugin = require( "html-webpack-plugin" );
 
 module.exports = {
     mode: "development",
-    entry: './src/js/index.js',
+    entry: ['babel-polyfill', './src/js/index.js'],
     output: {
         path: path.resolve( __dirname, 'dist' ),
         filename: 'js/bundle.js'
@@ -17,6 +17,19 @@ module.exports = {
             filename: "index.html",
             template: "./src/index.html"  // This helps inject html into the dist folder and observes changes in the index.js file
         } )],
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
+
 
 
 
